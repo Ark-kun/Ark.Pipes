e7eb15f277a795fd2d9b7574f6d74e3a4cca4121
Alexey Volkov <ark@ark-kun.com>
2009-08-24 00:01:02 +0000

    v5
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@7 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ _Test Projects/Test.XNAWindowsGame/Bullets/HomingBulletFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Test.XNAWindowsGame.SpriteTypes;
+
+namespace Test.XNAWindowsGame.Bullets
+{
+    class HomingBulletFactory
+    {
+        Texture2D _bulletSprite;
+        Vector2 _spriteOrigin;
+        Rectangle _originsSet;
+        float _maxSpeed;
+        Vector2 _target = Vector2.Zero;
+        Random rnd = new Random(666);
+
+        public HomingBulletFactory(Texture2D bulletSprite, Rectangle originsSet, float maxSpeed)
+        {
+            _bulletSprite = bulletSprite;
+            _originsSet = originsSet;
+            _maxSpeed = maxSpeed;
+            _spriteOrigin = new Vector2(_bulletSprite.Width / 2, _bulletSprite.Height / 2);
+        }
+
+        
+        public IGameElement GenerateBullet()
+        {
+            Vector2 origin = new Vector2(_originsSet.X + rnd.Next(_originsSet.Width), _originsSet.Y + rnd.Next(_originsSet.Height));
+            Vector2 direction = _target - origin;
+            direction.Normalize();
+            float speed = (float)(rnd.NextDouble() * _maxSpeed);
+            direction *= speed;
+            
+            return new StraitLineBullet<StaticSprite>(new StaticSprite(_bulletSprite, _spriteOrigin), origin, direction);
+        }
+        public Vector2 Target
+        {
+            get
+            {
+                return _target;
+            }
+            set
+            {
+                _target = value;
+            }
+        }
+
+    }
+}
