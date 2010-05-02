a484b9eb36e07d2d9983d3025bf4117d68e8002a
Alexey Volkov <ark@ark-kun.com>
2010-05-02 05:09:25 +0000

    Extracted TransformedSprite.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@35 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Shohou Project/SpriteTypes/TransformedSprite.cs
+++ Shohou Project/SpriteTypes/TransformedSprite.cs
@@ -3,23 +3,29 @@ using Microsoft.Xna.Framework.Graphics;
 using Ark.Pipes;
 
 namespace Ark.XNA.Sprites {
-    public class DynamicSprite : DrawableGameComponent, IHasChangeablePosition, IHasChangeableAngle, IHasChangeableTexture {
+    public class TransformedSprite : DrawableGameComponent, IHasChangeableTexture {
         private SpriteBatch _spriteBatch;
 
-        public DynamicSprite(Game game)
+        public TransformedSprite(Game game)
             : base(game) {
             _spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
 
-            Position = Vector2.Zero;
+            Transform = Ark.XNA.Transforms.Transform<Vector2>.Identity;
             Origin = Vector2.Zero;
             Texture = Constant<Texture2D>.Default;
-            Angle = 0;
-            Scale = 1;
             Tint = Color.White;
         }
 
         public void Draw() {
-            _spriteBatch.Draw(Texture, Position, null, Tint, Angle, Origin, Scale, SpriteEffects.None, 0);
+            var trans = Transform;
+            Texture2D tex = Texture;
+            var p00 = trans.Transform(Vector2.Zero);
+            var p01 = trans.Transform(new Vector2(0, tex.Height));
+            var p10 = trans.Transform(new Vector2(tex.Width, 0));
+            var p11 = trans.Transform(new Vector2(tex.Width, tex.Height));
+            var dst = new Rectangle((int)p00.X, (int)p00.Y, (int)((p10 - p00).Length()), (int)((p01 - p00).Length()));
+            var angle = (float)(System.Math.Atan2(p11.X - p00.X, p11.Y - p00.Y) - System.Math.Atan2(tex.Width, tex.Height));
+            _spriteBatch.Draw(tex, dst, null, Tint, angle, Origin, SpriteEffects.None, 0);
         }
 
         //public void Draw(Vector2 position) {
@@ -38,11 +44,9 @@ namespace Ark.XNA.Sprites {
         //    _spriteBatch.Draw(Texture, position, null, tint, angle, Origin, scale, SpriteEffects.None, 0);
         //}
 
-        public Provider<Vector2> Position { get; set; }
+        public ITransform<Vector2> Transform { get; set; }
         public Provider<Vector2> Origin { get; set; }
         public Provider<Texture2D> Texture { get; set; }
-        public Provider<float> Angle { get; set; }
-        public Provider<float> Scale { get; set; }
         public Provider<Color> Tint { get; set; }
 
         public override void Draw(GameTime gameTime) {
