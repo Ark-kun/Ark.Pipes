bf2f7eefa613a8d3a147ff14602f6450e669d7b4
Alexey Volkov <ark@ark-kun.com>
2011-09-09 00:33:07 +0000

    Ark.Pipes: Moved the Mouse providers to a new Ark.Pipes.Xna library.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@48 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes.Testing/Ark.Pipes.Tests.cs
+++ Ark.Pipes/Ark.Pipes.Testing/Ark.Pipes.Tests.cs
@@ -12,7 +12,7 @@ namespace Ark.Pipes.Testing {
             var rndInt = (Provider<int>)(() => (int)(1000 * rndDouble.RandomValue));
             var v3 = rndInt.Value;
             int v4 = rndInt;
-            rndInt = 666;
+            //rndInt = 666;
             int v42 = rndInt;
 
             var rndVector2 = (Provider<Vector2>)(() => new Vector2(rndInt, rndInt));
@@ -26,9 +26,9 @@ namespace Ark.Pipes.Testing {
             //var rndDoubleScaled = (Provider<double>)((k) => k * rndDouble.RandomValue);
             //var v1 = rndDouble.RandomValue.Value;
             //double v2 = rndDouble.RandomValue;
-            rndDouble = rndDouble;
+            //rndDouble = rndDouble;
 
-
+            //generic operators
             Func<int, string> ff1 = i => i.ToString();
             //Provider<string> p11 = (Provider<string>)ff1;
             Provider<string> p12 = Provider<string>.op_Implicit(ff1);
@@ -37,10 +37,30 @@ namespace Ark.Pipes.Testing {
             Provider<string> p21 = (Provider<string>)ff2;
             Provider<string> p22 = Provider<string>.op_Implicit(ff2);
 
+            //Vector2D
             var v2d1 = new Vector2D(7, 13);
+            v2d1.Length.Changed += () => { rndInt = 666; };
             double l1 = v2d1.Length;
-            v2d1.X = 666;
+            v2d1.X = rndDouble.RandomValue;
             double l2 = v2d1.Length;
+
+            v2d1.X = new Function<double>(() => 1);
+
+            //ReadOnlyProperty
+            //ReadOnlyProperty<int> rop = rndInt;
+            //var rop = new ReadOnlyProperty<int>(rndInt);
+            //int v9 = rop;
+            ////rop = 134;
+            //int v10 = rop;
+
+            //Property<int> prop = rndInt;
+            ////rop = prop;
+            //prop = rop;
+            
+            //Interface conversion
+            //Provider<int> pr2 = (Provider<int>)(IOut<int>)(new Const<int>(13));
+            //IOut<int> iout1 = 13;
+            //ReadOnlyProperty<int> rop2 = (IOut<int>)null;
         }
     }
 
@@ -56,26 +76,6 @@ namespace Ark.Pipes.Testing {
             get {
                 return _rv;
             }
-        }
-    }
-
-    public class Mouse {
-        static Provider<Vector2> _p = (Func<Vector2>)(() => {
-            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
-            return new Vector2(mouseState.X, mouseState.Y);
-        });
-
-        public static Provider<Vector2> Position {
-            get {
-                return _p;
-            }
-        }
-    }
-
-    public class MousePosition : Provider<Vector2> {
-        public override Vector2 GetValue() {
-            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
-            return new Vector2(mouseState.X, mouseState.Y);
         }
     }
 }
---
+++ Ark.Pipes/Ark.Pipes.Xna/Mouse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Ark.Pipes.Xna {
+    public static class Mouse {
+        static Provider<Vector2> _p = new Function<Vector2>(GetPosition);
+
+        static Vector2 GetPosition() {
+            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
+            return new Vector2(mouseState.X, mouseState.Y);
+        }
+
+        public static Provider<Vector2> Position {
+            get {
+                return _p;
+            }
+        }
+    }
+
+    public class MousePosition : Provider<Vector2> {
+        public override Vector2 GetValue() {
+            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
+            return new Vector2(mouseState.X, mouseState.Y);
+        }
+    }
+}
