2b553dddecd328c41857ace5fe16e9a144c1dbd8
Alexey Volkov <ark@ark-kun.com>
2011-09-14 19:24:55 +0000

    Ark.Pipes: Added TwoWayBinding.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@52 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/TwoWayBinding.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ark.Pipes {
+
+    //OneTime: Target.Property = Source.Property.Value
+    //OneWay: Target.Property = Source.Property
+    //OneWayToSource: Source.Property = Target.Property
+    //TwoWay: new TwoWayBinding(Source.Property, Target.Property)
+
+    public abstract class TwoWayBindingBase<TSource, TTarget> {
+        protected Property<TSource> _source;
+        protected Property<TTarget> _target;
+
+        public TwoWayBindingBase(Property<TSource> source, Property<TTarget> target) {
+            _source = source;
+            _target = target;
+
+            _source.ProviderChanged += OnSourceProviderChanged;
+            _target.ProviderChanged += OnTargetProviderChanged;            
+        }
+
+        protected abstract void OnSourceProviderChanged();
+        protected abstract void OnTargetProviderChanged();
+    }
+
+    public class TwoWayBinding<T> : TwoWayBindingBase<T, T> {
+        public TwoWayBinding(Property<T> source, Property<T> target)
+            : base(source, target) {
+                OnSourceProviderChanged();
+        }
+
+        protected override void OnSourceProviderChanged() {
+            _target.Provider = _source.Provider;
+        }
+
+        protected override void OnTargetProviderChanged() {
+            _source.Provider = _target.Provider;
+        }
+    }
+
+    public class TwoWayBinding<TSource, TTarget> : TwoWayBindingBase<TSource, TTarget> {
+        Provider<TSource> _lastSource;
+        Provider<TTarget> _lastTarget;
+        Func<TSource, TTarget> _sourceToTarget;
+        Func<TTarget, TSource> _targetToSource;
+
+        public TwoWayBinding(Property<TSource> source, Property<TTarget> target, Func<TSource, TTarget> sourceToTarget, Func<TTarget, TSource> targetToSource)
+            : base(source, target) {
+            _sourceToTarget = sourceToTarget;
+            _targetToSource = targetToSource;
+
+            OnSourceProviderChanged();
+        }
+
+        protected override void OnSourceProviderChanged() {
+            if (_lastSource == null || _source.Provider != _lastSource) {
+                _lastTarget = new Function<TSource, TTarget>(_sourceToTarget, _source.Provider);
+                _target.Provider = _lastTarget;
+            }
+        }
+
+        protected override void OnTargetProviderChanged() {
+            if (_lastTarget == null || _target.Provider != _lastTarget) {
+                _lastSource = new Function<TTarget, TSource>(_targetToSource, _target.Provider);
+                _source.Provider = _lastSource;
+            }
+        }
+    }
+}
