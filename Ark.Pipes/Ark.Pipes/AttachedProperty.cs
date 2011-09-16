67f86d1b515c7ab30025902698fb7512b2b69499
Alexey Volkov <ark@ark-kun.com>
2011-09-16 00:58:45 +0000

    Ark.Pipes: Added AttachedProperty<>.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@53 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/AttachedProperty.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Ark.Pipes {
+
+    //Canvas.Left[button1] = 13
+    //button1[Canvas.Left] = 13 //?
+    public class AttachedProperty<T> {
+        Dictionary<object, Provider<T>> _store = new Dictionary<object, Provider<T>>();
+
+        public Provider<T> this[object obj] {
+            get { return _store[obj]; }
+            set { _store[obj] = value; }
+        }
+    }
+}
