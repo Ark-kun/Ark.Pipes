a1a701dfa335d11ea8e2ba7b2bfecbe55d180efe
Alexey Volkov <ark@ark-kun.com>
2011-09-18 05:02:51 +0000

    Ark.Pipes: Added Functon<> with 3 arguments.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@54 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes/Function.cs
+++ Ark.Pipes/Ark.Pipes/Function.cs
@@ -84,4 +84,50 @@ namespace Ark.Pipes {
             }
         }
     }
+
+    public class Function<T1, T2, T3, TResult> : Provider<TResult> {
+        private Func<T1, T2, T3, TResult> _function;
+        private Property<T1> _arg1;
+        private Property<T2> _arg2;
+        private Property<T3> _arg3;
+
+        public Function(Func<T1, T2, T3, TResult> function)
+            : this(function, Constant<T1>.Default, Constant<T2>.Default, Constant<T3>.Default) {
+        }
+
+        public Function(Func<T1, T2, T3, TResult> function, Provider<T1> arg1, Provider<T2> arg2, Provider<T3> arg3) {
+            _function = function;
+            _arg1 = new Property<T1>(arg1);
+            _arg2 = new Property<T2>(arg2);
+            _arg3 = new Property<T3>(arg3);
+            _arg1.ValueChanged += OnValueChanged;
+            _arg2.ValueChanged += OnValueChanged;
+            _arg3.ValueChanged += OnValueChanged;
+        }
+
+        public override TResult GetValue() {
+            return _function(_arg1.Value, _arg2.Value, _arg3.Value);
+        }
+
+        public Property<T1> Argument1 {
+            get { return _arg1; }
+            set { _arg1.Provider = value.Provider; }
+        }
+
+        public Property<T2> Argument2 {
+            get { return _arg2; }
+            set {
+                _arg2.Provider = value.Provider;
+                OnValueChanged();
+            }
+        }
+
+        public Property<T3> Argument3 {
+            get { return _arg3; }
+            set {
+                _arg3.Provider = value.Provider;
+                OnValueChanged();
+            }
+        }
+    }
 }
