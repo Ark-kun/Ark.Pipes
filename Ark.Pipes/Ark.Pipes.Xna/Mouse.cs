9e4d28b53a23038a2db7d6464deb759247d7bc52
Alexey Volkov <ark@ark-kun.com>
2011-09-20 02:41:37 +0000

    Ark.Pipes.Xna: Renamed Mouse to XnaMouse and had it implement IMouse<>.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@60 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes.Xna/Mouse.cs
+++ Ark.Pipes/Ark.Pipes.Xna/Mouse.cs
@@ -1,19 +1,50 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using Ark.Pipes.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Ark.Pipes.Xna {
-    public static class Mouse {
-        static Provider<Vector2> _p = new Function<Vector2>(GetPosition);
+    public class XnaMouse : IMouse<Vector2> {
+        static Provider<Vector2> _position = new Function<Vector2>(GetPosition);
+        static Provider<bool> _leftButton = new Function<bool>(() => Mouse.GetState().LeftButton == ButtonState.Pressed);
+        static Provider<bool> _middleButton = new Function<bool>(() => Mouse.GetState().MiddleButton == ButtonState.Pressed);
+        static Provider<bool> _rightButton = new Function<bool>(() => Mouse.GetState().RightButton == ButtonState.Pressed);
 
         static Vector2 GetPosition() {
-            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
+            var mouseState = Mouse.GetState();
             return new Vector2(mouseState.X, mouseState.Y);
         }
 
         public static Provider<Vector2> Position {
-            get {
-                return _p;
-            }
+            get { return _position; }
+        }
+
+        public static Provider<bool> IsLeftButtonPressed {
+            get { return _leftButton; }
+        }
+
+        public static Provider<bool> IsMiddleButtonPressed {
+            get { return _middleButton; }
+        }
+
+        public static Provider<bool> IsRightButtonPressed {
+            get { return _rightButton; }
+        }
+
+
+        Provider<Vector2> IMouse<Vector2>.Position {
+            get { return _position; }
+        }
+
+        Provider<bool> IMouse<Vector2>.IsLeftButtonPressed {
+            get { return _leftButton; }
+        }
+
+        Provider<bool> IMouse<Vector2>.IsMiddleButtonPressed {
+            get { return _middleButton; }
+        }
+
+        Provider<bool> IMouse<Vector2>.IsRightButtonPressed {
+            get { return _rightButton; }
         }
     }
 
