44caa472c2ca2450f6f6a3c4c3d4298f3d34f150
Alexey Volkov <ark@ark-kun.com>
2011-09-20 02:31:15 +0000

    Ark.Pipes.Input: Initial commit. Added IMouse<> interface.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@58 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes.Input/Mouse.cs
@@ -0,0 +1,8 @@
+﻿namespace Ark.Pipes.Input {
+    public interface IMouse<TPoint> {
+        Provider<TPoint> Position { get; }
+        Provider<bool> IsLeftButtonPressed { get; }
+        Provider<bool> IsMiddleButtonPressed { get; }
+        Provider<bool> IsRightButtonPressed { get; }
+    }
+}
