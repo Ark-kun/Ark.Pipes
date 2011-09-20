eb80db7fb263995a64925af1e65c75c62211b9e0
Alexey Volkov <ark@ark-kun.com>
2011-09-20 02:41:57 +0000

    Ark.Pipes: Added NotifyPropertyChangedAdapter.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@61 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/NotifyPropertyChangedAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace Ark.Pipes.Wpf {
+    public class NotifyPropertyChangedAdapter<T> : INotifyPropertyChanged {
+        const string _propertyName = "Value";
+        Provider<T> _value;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NotifyPropertyChangedAdapter(Provider<T> provider) {
+            _value = provider;
+            _value.ValueChanged += OnPropertyChanged;
+            OnPropertyChanged();
+        }
+
+        public Provider<T> Value {
+            get { return _value; }
+            set {
+                if (value != _value) {
+                    _value.ValueChanged -= OnPropertyChanged;
+                    _value = value;
+                    _value.ValueChanged += OnPropertyChanged;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected void OnPropertyChanged() {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(_propertyName));
+            }
+        }
+    }
+}
