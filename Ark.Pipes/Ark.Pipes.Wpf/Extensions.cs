dd14abec825ce1233d4177b626a1b8492b83e530
Alexey Volkov <ark@ark-kun.com>
2011-09-24 03:47:57 +0000

    Ark.Pipes.Wpf: Fixed ToVector3Component extension method.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@65 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes.Wpf/Extensions.cs
+++ Ark.Pipes/Ark.Pipes.Wpf/Extensions.cs
@@ -2,13 +2,12 @@
 using System.Windows.Media.Media3D;
 
 namespace Ark.Pipes.Wpf {
-    static class Extensions {
-
+    public static class Extensions {
         public static Vector2Components ToVector2Components(this Provider<Point> point) {
             return new Vector2Components(new Function<double>(() => point.Value.X), new Function<double>(() => point.Value.Y));
         }
 
-        public static Vector3Components ToVector2Component(Provider<Vector3D> point) {
+        public static Vector3Components ToVector3Component(this Provider<Vector3D> point) {
             return new Vector3Components(new Function<double>(() => point.Value.X), new Function<double>(() => point.Value.Y), new Function<double>(() => point.Value.Z));
         }
     }
