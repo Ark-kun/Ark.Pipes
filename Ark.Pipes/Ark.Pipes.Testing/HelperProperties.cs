743f799143ca3649202df60d589224a91d6ffea6
Alexey Volkov <ark@ark-kun.com>
2012-02-07 04:33:24 +0000

    Added the Name attached property.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@69 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes.Testing/HelperProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ark.Pipes.Testing {
+    public static class HelperProperties {
+        static AttachedProperty<string> _name = new AttachedProperty<string>();
+
+        public static AttachedProperty<string> Name {
+            get { return _name; }
+        }
+    }
+}
