200b8b41a1de2161d34e6da416fabbb9beefc11b
Alexey Volkov <ark@ark-kun.com>
2012-02-07 04:32:59 +0000

    Added PropertyConverter.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@68 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes.Testing/PropertyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Ark.Pipes {
+    public class PropertyConverter : TypeConverter {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType) {
+            return true;
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType) {
+            return destinationType == typeof(Property<>);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value) {            
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType) {
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
