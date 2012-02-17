61bb2c17ed550d75863bf05f2b16accc92f2695e
Alexey Volkov <ark@ark-kun.com>
2012-02-17 23:55:42 +0000

    Added assembly info.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@85 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ark.Pipes")]
+[assembly: AssemblyDescription("Ark.Pipes library makes everything a dynamic data source.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Flying Ark havens")]
+[assembly: AssemblyProduct("Ark.Pipes")]
+[assembly: AssemblyCopyright("©Flying Ark havens 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("117a4a8a-bf68-496a-9a4c-7a524998d9fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.1.*")]
