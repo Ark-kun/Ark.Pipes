3d2342dd81ecd81cf58e8c64a4f551ef20ea4be2
Alexey Volkov <ark@ark-kun.com>
2012-02-18 02:08:25 +0000

    Removed non-portable COM attributes.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@86 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes/Properties/AssemblyInfo.cs
+++ Ark.Pipes/Ark.Pipes/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("117a4a8a-bf68-496a-9a4c-7a524998d9fd")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
