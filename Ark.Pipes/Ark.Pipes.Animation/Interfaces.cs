9751c99b856949bc0525521d7396cf4e1f946a12
Alexey Volkov <ark@ark-kun.com>
2012-02-19 22:40:33 +0000

    Renamed assemblies, projects and directories. Part 3
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@115 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes.Animation/Interfaces.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ark.Animation {
-    public interface IHasPosition<T> {
-        T Position { get; set; }
-    }
-
-    public interface IHasOrientation<T> {
-        T Orientation { get; set; }
-    }
-}
--- Ark.Pipes/Ark.Pipes.Wpf.Testing/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Ark.Pipes.Wpf.Testing {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
-    }
-}
