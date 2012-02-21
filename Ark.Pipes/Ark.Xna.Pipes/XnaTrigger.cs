b9c9b468d52f7aa8353b2fc8aa230ec8dc3579ae
Alexey Volkov <ark@ark-kun.com>
2012-02-21 12:07:46 +0000

    Forgot to Add the XnaTrigger class.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@125 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Xna.Pipes/XnaTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ark.Pipes;
+using Microsoft.Xna.Framework;
+
+namespace Ark.Animation.Pipes.Xna {
+    public class XnaTrigger : GameComponent, ITrigger {
+        public event Action Triggered;
+
+        public XnaTrigger(Game game) : base(game) { 
+        }
+
+        public override void Update(GameTime gameTime) {
+            var handler = Triggered;
+            if (handler != null) {
+                handler();
+            }
+        }
+    }
+}
