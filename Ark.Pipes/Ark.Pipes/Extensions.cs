169c6757fde39baea7ae48fbd2823a6261407fe6
Alexey Volkov <ark@ark-kun.com>
2012-02-22 04:22:03 +0000

    Added Extensions class to Ark.Pipes. It only has AsEnumerable method for now. Need to implement AsObservable.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@129 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ark.Pipes {
+    public static class Extensions {
+        public static IEnumerable<T> AsEnumerable<T>(this IOut<T> provider) {
+            while (true) {
+                yield return provider.GetValue();
+            }
+        }
+
+#if false
+        public IEnumerable<T> AsEnumerableChanges<T>(this INotifyingOut<T> provider) {
+            var trigger = new ManualResetEvent(false);
+            Action onChanged = () => trigger.Set();
+            provider.Notifier.ValueChanged += onChanged;
+            while (true) {
+                yield return provider.GetValue();
+                if (provider.Notifier.IsReliable) {
+                    trigger.Reset();
+                }
+            }
+            provider.Notifier.ValueChanged -= onChanged;
+        }
+#endif
+    }
+}
