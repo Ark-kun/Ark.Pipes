a790f1bbf2a752bd5e4e4cc04bf2823add90b9b6
Alexey Volkov <ark@ark-kun.com>
2012-03-04 04:12:00 +0000

    Added new triggers: SwitchableTrigger and PeriodicTrigger.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@149 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Animation.Pipes/PeriodicTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ark.Pipes;
+
+#if FLOAT_TYPE_DOUBLE
+using TFloat = System.Double;
+#else
+using TFloat = System.Single;
+#endif
+
+namespace Ark.Animation {
+    public class PeriodicTrigger : TriggerBase {
+        TFloat _interval;
+        TFloat _lastTime;
+        Provider<TFloat> _timer;
+
+#if !NOTIFICATIONS_DISABLE
+        public PeriodicTrigger(float interval, Provider<float> timer) {
+            _interval = interval;
+            _timer = timer;
+            _lastTime = _timer.Value;
+            _timer.Notifier.ValueChanged += OnTick;
+        }
+#endif
+        public PeriodicTrigger(TFloat interval, Provider<TFloat> timer, ITrigger changeTrigger)
+            : this(interval, 0, timer, changeTrigger) {
+        }
+
+        public PeriodicTrigger(TFloat interval, TFloat waitTime, Provider<TFloat> timer, ITrigger changeTrigger) {
+            _interval = interval;
+            _timer = timer;
+            _lastTime = _timer.Value + waitTime;
+            changeTrigger.Triggered += OnTick;
+        }
+
+        void OnTick() {
+            TFloat time = _timer.Value;
+            while (time - _lastTime > _interval) {
+                OnTriggered();
+                _lastTime += _interval;
+            }
+        }
+    }
+}
---
+++ Ark.Pipes/Ark.Animation.Pipes/SwitchableTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ark.Pipes;
+
+namespace Ark.Animation {
+    public sealed class SwitchableTrigger : TriggerBase {
+        Provider<bool> _switcher;
+
+        public SwitchableTrigger(Provider<bool> switcher, ITrigger trigger) {
+            _switcher = switcher;
+            trigger.Triggered += OnTriggerTriggered;
+        }
+
+        void OnTriggerTriggered() {
+            if (_switcher.Value) {
+                OnTriggered();
+            }
+        }
+    }
+}
