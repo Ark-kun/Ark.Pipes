199ca51155d5b71787a67597c3cd60b4a1c2dd5a
Alexey Volkov <ark@ark-kun.com>
2012-03-17 02:10:18 +0000

    Added CustomMovement factory class for the (state) -> newState (state, t) -> newState (state, dt) -> newState (state, t, dt) -> newState movements.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@183 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Animation.Pipes/Curves/CustomMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Ark.Animation;
+using Ark.Pipes;
+
+#if FLOAT_TYPE_DOUBLE
+using TFloat = System.Double;
+#else
+using TFloat = System.Single;
+#endif
+
+namespace Ark.Geometry.Curves {
+    public static class CustomMovement {
+        public static Provider<T> Create<T>(Func<T, T> changer, T initialState) { 
+            T state = initialState;
+            return Provider.Create(() => {
+                state = changer(state);
+                return state;
+            });
+        }
+
+        public static Provider<T> Create<T>(Func<T, DeltaT, T> changer, T initialState, Provider<TFloat> timer) {
+            return Create(changer, initialState, timer.ToDeltaTs());
+        }
+
+        public static Provider<T> Create<T>(Func<T, DeltaT, T> changer, T initialState, Provider<DeltaT> deltaTs) {
+            T state = initialState;
+            return Provider.Create((dt) => {
+                state = changer(state, dt);
+                return state;
+            }, deltaTs);
+        }
+
+        public static Provider<T> Create<T>(Func<T, TFloat, T> changer, T initialState, Provider<TFloat> timer) {
+            T state = initialState;
+            return Provider.Create((t) => {
+                state = changer(state, t);
+                return state;
+            }, timer);
+        }
+
+        public static Provider<T> Create<T>(Func<T, TFloat, DeltaT, T> changer, T initialState, Provider<TFloat> timer) {
+            T state = initialState;
+            TFloat lastTime = timer.Value;
+            return Provider.Create((t) => {
+                state = changer(state, t, t - lastTime);
+                lastTime = t;
+                return state;
+            }, timer);
+        }
+    }
+}
