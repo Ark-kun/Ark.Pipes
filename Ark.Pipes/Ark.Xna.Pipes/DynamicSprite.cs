c0732b8312cce0d2925d97d433e3c5d1cd4fad62
Alexey Volkov <ark@ark-kun.com>
2012-08-17 00:12:31 +0000

    Made DynamicSprite use Property<> instead of Provider<> and added explicit caching (was that needed?)
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@195 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Xna.Pipes/DynamicSprite.cs
+++ Ark.Pipes/Ark.Xna.Pipes/DynamicSprite.cs
@@ -1,4 +1,5 @@
-﻿using Ark.Pipes;
+﻿using Ark.Geometry;
+using Ark.Pipes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,12 +7,23 @@ namespace Ark.Graphics { //.Pipes.Xna {
     public class DynamicSprite : DrawableGameComponent {
         private SpriteBatch _spriteBatch;
 
+        Property<Vector2> _position = new Property<Vector2>();
+        Property<Texture2D> _texture = new Property<Texture2D>();
+        Property<float> _angle = new Property<float>();
+        Property<float> _scale = new Property<float>();
+        Property<Color> _tint = new Property<Color>();
+
+        Vector2 _positionCache;
+        Texture2D _textureCache;
+        float _angleCache;
+        float _scaleCache;
+        Color _tintCache;
+
         public DynamicSprite(Game game)
             : base(game) {
             _spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
 
             Position = Vector2.Zero;
-            Origin = Vector2.Zero;
             Texture = Constant<Texture2D>.Default;
             Angle = 0;
             Scale = 1;
@@ -19,28 +31,47 @@ namespace Ark.Graphics { //.Pipes.Xna {
         }
 
         public void Draw() {
-            _spriteBatch.Draw(Texture, Position, null, Tint, Angle, Origin, Scale, SpriteEffects.None, 0);
+            _spriteBatch.Draw(_textureCache, _positionCache, null, _tintCache, _angleCache, _textureCache.Center(), _scaleCache, SpriteEffects.None, 0);
         }
 
-        public Provider<Vector2> Position { get; set; }
-        public Provider<Vector2> Origin { get; set; }
-        public Provider<Texture2D> Texture { get; set; }
-        public Provider<float> Angle { get; set; }
-        public Provider<float> Scale { get; set; }
-        public Provider<Color> Tint { get; set; }
+        public Property<Vector2> Position {
+            get { return _position; }
+            set { _position.Provider = value; }
+        }
 
+        public Property<Texture2D> Texture {
+            get { return _texture; }
+            set { _texture.Provider = value; }
+        }
+
+        public Property<float> Angle {
+            get { return _angle; }
+            set { _angle.Provider = value; }
+        }
+
+        public Property<float> Scale {
+            get { return _scale; }
+            set { _scale.Provider = value; }
+        }
+
+        public Property<Color> Tint {
+            get { return _tint; }
+            set { _tint.Provider = value; }
+        }
+        
         public override void Draw(GameTime gameTime) {
             base.Draw(gameTime);
             Draw();
         }
 
-        public delegate void GameTimeEvent(GameTime gameTime);
-        public event GameTimeEvent Updated;
         public override void Update(GameTime gameTime) {
+            _positionCache = _position;
+            _textureCache = _texture;
+            _angleCache = _angle;
+            _scaleCache = _scale;
+            _tintCache = _tint;
+
             base.Update(gameTime);
-            if (Updated != null) {
-                Updated(gameTime);
-            }
         }
     }
 }
