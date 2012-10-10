5e3a413471d45465484511022c1c6bd037e1f497
Alexey Volkov <ark@ark-kun.com>
2012-10-10 21:24:44 +0000

    Added the SwitchableProvider classes.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@232 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/SwitchableProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ark.Pipes {
+    public static class SwitchableProvider {
+        public static Provider<TValue> Create<TKey, TValue>(Provider<TKey> selector, Dictionary<TKey, Provider<TValue>> providers) {
+            return new SwitchableProvider<TKey, TValue>(selector, providers);
+        }
+
+        public static Provider<TValue> Create<TValue>(Provider<int> selector, IList<Provider<TValue>> providers) {
+            return new SwitchableProvider<TValue>(selector, providers);
+        }
+
+        public static Provider<TValue> Switch<TKey, TValue>(this Provider<TKey> selector, Dictionary<TKey, Provider<TValue>> providers) {
+            return new SwitchableProvider<TKey, TValue>(selector, providers);
+        }
+
+        public static Provider<TValue> Switch<TValue>(this Provider<int> selector, IList<Provider<TValue>> providers) {
+            return new SwitchableProvider<TValue>(selector, providers);
+        }
+    }
+
+    sealed class SwitchableProvider<TKey, TValue> : ReadableProvider<TValue> {
+        Provider<TKey> _selector;
+        TKey _currentKey;
+        IDictionary<TKey, Provider<TValue>> _providers;
+
+        public SwitchableProvider(Provider<TKey> selector)
+            : base(Constant<TValue>.Default) {
+            _selector = selector;
+            _providers = new Dictionary<TKey, Provider<TValue>>();
+        }
+
+        public SwitchableProvider(Provider<TKey> selector, Dictionary<TKey, Provider<TValue>> providers)
+            : base(providers[selector]) {
+            _selector = selector;
+            _currentKey = _selector;
+            _providers = providers;
+            _selector.Notifier.ValueChanged += OnSelectorChanged;
+        }
+
+        void OnSelectorChanged() {
+            var key = _selector.Value;
+            Provider<TValue> provider;
+            //if (key != _currentKey) {
+            if (_providers.TryGetValue(key, out provider)) {
+                _currentKey = key;
+                SetProvider(provider);
+            } else {
+                //throw new InvalidOperationException("SwitchableProvider was switched to a non-existent provider.");            
+            }
+            //}
+        }
+    }
+
+    sealed class SwitchableProvider<TValue> : ReadableProvider<TValue> {
+        Provider<int> _selector;
+        int _currentKey;
+        IList<Provider<TValue>> _providers;
+
+        public SwitchableProvider(Provider<int> selector, int count)
+            : base(Constant<TValue>.Default) {
+            _selector = selector;
+            _providers = new Provider<TValue>[count];
+        }
+
+        public SwitchableProvider(Provider<int> selector, IList<Provider<TValue>> providers)
+            : base(providers[selector]) {
+            _selector = selector;
+            _currentKey = _selector;
+            _providers = providers;
+            _selector.Notifier.ValueChanged += OnSelectorChanged;
+        }
+
+        void OnSelectorChanged() {
+            var key = _selector.Value;            
+            //if (key != _currentKey) {
+            if (key >= 0 && key < _providers.Count) {
+                _currentKey = key;
+                var provider = _providers[_currentKey];
+                SetProvider(provider);
+            } else {
+                //throw new InvalidOperationException("SwitchableProvider was switched to a non-existent provider.");
+            }
+            //}
+        }
+    }
+}
