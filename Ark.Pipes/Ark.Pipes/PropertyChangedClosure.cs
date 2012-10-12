2edb0016f22fb62ce4b953bcd6033d4e28052b8d
Alexey Volkov <ark@ark-kun.com>
2012-10-12 00:35:03 +0000

    Extracted the PropertyChangedClosure class from the Provider<> class into it's own file.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@233 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/PropertyChangedClosure.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+#if !NOTIFICATIONS_DISABLE
+namespace Ark.Pipes {
+    class PropertyChangedClosure : IEquatable<PropertyChangedEventHandler>, IEquatable<PropertyChangedClosure>, IEquatable<Action> {
+        Object _sender;
+        PropertyChangedEventHandler _handler;
+        static PropertyChangedEventArgs _eventArgs = new PropertyChangedEventArgs("value");
+
+        public PropertyChangedClosure(PropertyChangedEventHandler handler, object sender) {
+            _handler = handler;
+            _sender = sender;
+        }
+
+        public void Invoke() {
+            _handler(_sender, _eventArgs);
+        }
+
+        public override bool Equals(object obj) {
+            var action = obj as Action;
+            if (action != null) {
+                return Equals(action);
+            }
+            var closure = obj as PropertyChangedClosure;
+            if (closure != null) {
+                return Equals(closure);
+            }
+            var handler = obj as PropertyChangedEventHandler;
+            if (handler != null) {
+                return Equals(handler);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return _handler.GetHashCode();
+        }
+
+        public bool Equals(PropertyChangedEventHandler other) {
+            return _handler == other;
+        }
+
+        public bool Equals(PropertyChangedClosure other) {
+            return _handler == other._handler;
+        }
+
+        public bool Equals(Action other) {
+            var closure = other.Target as PropertyChangedClosure;
+            return closure != null && closure._handler == _handler;
+        }
+    }
+}
+#endif
