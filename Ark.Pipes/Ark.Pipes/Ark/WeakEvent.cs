2c6676eb84791fa20250b027a4112f4819f63f26
Alexey Volkov <ark@ark-kun.com>
2012-10-19 05:49:33 +0000

    Moved the class files containing classes from the Ark namespace into the Ark folder.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@243 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Ark/WeakEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ark.Collections;
+
+namespace Ark {
+    class WeakEvent<TDelegate> {
+        WeakCollection<TDelegate> _handlers;
+
+        public WeakEvent() {
+            if (!typeof(Delegate).IsAssignableFrom(typeof(TDelegate)))
+                throw new ArgumentException("TDelegate must be a delegate type.");
+            //_handlers = new WeakCollection<TDelegate>((h) => WeakDelegate.Weaken(h), null);
+        }
+
+        public void Add(TDelegate handler) {
+        }
+
+        public void AddStrong(TDelegate handler) {
+        }
+
+        public void AddWeak(TDelegate handler) {
+        }
+
+        public void Remove(TDelegate handler) {
+        }
+    }
+}
---
+++ Ark.Pipes/Ark.Pipes/_not in v1/AttachedProperty.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Ark.Pipes {
+    //Canvas.Left[button1] = 13
+    //button1[Canvas.Left] = 13 //?
+    //TODO:Change notification
+    //FIX: memory leak
+    public sealed class AttachedProperty<T> {
+        Dictionary<object, Provider<T>> _store = new Dictionary<object, Provider<T>>();
+
+        public Provider<T> this[object obj] {
+            get { return _store[obj]; }
+            set { _store[obj] = value; }
+        }
+    }
+}
