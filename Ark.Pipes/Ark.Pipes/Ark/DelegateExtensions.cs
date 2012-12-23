e842b2e8d906d792a35b8229ab7da8b9a5aa929d
Alexey Volkov <ark@ark-kun.com>
2012-12-23 22:32:15 +0000

    Added DelegateExtensions class with methods to calculate a better hash code for delegates and to get the typed invocation list.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@262 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Ark/DelegateExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Ark {
+    static class DelegateExtensions {
+        static int GetGoodHashCodeInternal(this Delegate del) {
+            return del.Method.GetHashCode() ^ RuntimeHelpers.GetHashCode(del.Target);
+        }
+
+        public static int GetGoodHashCode(this Delegate del) {
+            var multicastDelegate = del as MulticastDelegate;
+            if (multicastDelegate != null) {
+                return GetGoodHashCode(multicastDelegate);
+            }
+            return GetGoodHashCodeInternal(del);
+        }
+
+        public static int GetGoodHashCode(this MulticastDelegate multicastDel) {
+            var delegates = multicastDel.GetInvocationList();
+            if (delegates == null || delegates.Length == 0) {
+                return multicastDel.GetType().GetHashCode();
+            } else {
+                int hash = 0;
+                foreach (var del in multicastDel.GetInvocationList()) {
+                    hash = hash * 0x21 + GetGoodHashCodeInternal(del);
+                }
+                return hash;
+            }
+        }
+
+        public static IEnumerable<TDelegate> GetTypedInvocationList<TDelegate>(this TDelegate handler) where TDelegate : class {
+            var del = handler as Delegate;
+            if (del == null) {
+                throw new ArgumentException("The TDelegate generic parameter must be a delegate type.");
+            }
+            var delegates = del.GetInvocationList();
+
+            return delegates.OfType<TDelegate>();
+        }
+    }
+}
