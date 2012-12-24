1feb2efad2143e05f73057f1d5ee7e2bed8dfc23
Alexey Volkov <ark@ark-kun.com>
2012-12-24 02:17:06 +0000

    Added the EmptyEnumerator<> class.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@264 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Collections/EmptyEnumerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ark.Collections {
+    struct EmptyEnumerator<T> : IEnumerator<T> {
+        static EmptyEnumerator<T> _instance = new EmptyEnumerator<T>();
+
+        public T Current {
+            get { throw new InvalidOperationException("EmptyEnumerator.Current was called."); }
+        }
+
+        public void Dispose() { }
+
+        object System.Collections.IEnumerator.Current {
+            get { return Current; }
+        }
+
+        public bool MoveNext() {
+            return false;
+        }
+
+        public void Reset() { }
+
+        public static EmptyEnumerator<T> Instance {
+            get { return _instance; }
+        }
+    }
+}
