e42e1f21c36c0da589c5f0ce3a929822df1af7e2
Alexey Volkov <ark@ark-kun.com>
2012-12-24 02:24:02 +0000

    Added the DictionaryExtensions class which simplifies getting values from a dictionary. (Forgot to add.)
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@266 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Collections/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Ark.Collections {
+    static class DictionaryExtensions {
+        public static TValue GetOrCreateValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : class, new() {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value)) {
+                value = new TValue();
+                dictionary.Add(key, value);
+            }
+            return value;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) {
+            TValue value;
+            if (dictionary.TryGetValue(key, out value)) {
+                return value;
+            } else {
+                return default(TValue);
+            }
+        }
+    }
+}
