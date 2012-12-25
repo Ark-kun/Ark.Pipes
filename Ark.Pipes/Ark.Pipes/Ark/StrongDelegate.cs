daec7f6897086977ec9e6ef470729f878ffb9b98
Alexey Volkov <ark@ark-kun.com>
2012-12-25 03:44:15 +0000

    Added the StrongDelegate<> class which derives from the SingleDelegate<> class and stores a normal delegate instance.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@275 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Pipes/Ark/StrongDelegate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ark {
+    class StrongDelegate<TDelegate> : SingleDelegate<TDelegate> where TDelegate : class {
+        TDelegate _handler;
+
+        public StrongDelegate(TDelegate handler)
+            : base(handler) {
+            if ((object)handler == null) {
+                throw new ArgumentNullException("handler must not be null");
+            }
+            var delegateHandler = handler as Delegate;
+            if (delegateHandler == null)
+                throw new ArgumentException("Agrument must have a delegate type.");
+
+            _handler = handler; //TODO: take only the first handler
+        }
+
+        public override object Target {
+            get { return Delegate.Target; }
+        }
+
+        public override MethodInfo Method {
+            get { return Delegate.Method; }
+        }
+
+        public override TDelegate Invoke {
+            get { return _handler; }
+        }
+
+        Delegate Delegate {
+            get { return _handler as Delegate; }
+        }
+
+        public override bool TryDynamicInvoke(object[] args) {
+            Delegate.DynamicInvoke(args);
+            return true;
+        }
+    }
+}
