d1977592db311c17c53710f1b4387ae8fa9caaf5
Alexey Volkov <ark@ark-kun.com>
2013-01-12 10:34:06 +0000

    Added the WeaklyReferencedStrongDelegate<> class which sores a weak reference to a normal delegate (the delegate target needs to reference that delegate to prevent its collection by GC).
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@284 94022cc4-76a7-104b-a55e-098ab84f6d6f

---
+++ Ark.Pipes/Ark.Weakness/Ark/WeaklyReferencedStrongDelegate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ark {
+    class WeaklyReferencedStrongDelegate<TDelegate> : SingleDelegate<TDelegate> where TDelegate : class {
+        WeakReference _handlerReference;
+
+        public WeaklyReferencedStrongDelegate(TDelegate handler)
+            : base(handler) {
+            if ((object)handler == null) {
+                throw new ArgumentNullException("handler must not be null");
+            }
+            var delegateHandler = handler as Delegate;
+            if (delegateHandler == null)
+                throw new ArgumentException("Agrument must have a delegate type.");
+
+            _handlerReference = new WeakReference(handler); //TODO: take only the first handler
+        }
+
+        public override object Target {
+            get {
+                var del = Delegate;
+                return del != null ? del.Target : null;
+            }
+        }
+
+        public override MethodInfo Method {
+            get {
+                var del = Delegate;
+                return del != null ? del.Method : null;
+            }
+        }
+
+        public override TDelegate Invoke {
+            get { return (TDelegate)_handlerReference.Target; }
+        }
+
+        Delegate Delegate {
+            get { return (Delegate)_handlerReference.Target; }
+        }
+
+        public override bool TryDynamicInvoke(object[] args, out object result) {
+            result = DynamicInvoke(args);
+            return true;
+        }
+
+        public override object DynamicInvoke(params object[] args) {
+            return Delegate.DynamicInvoke(args);
+        }
+    }
+}
