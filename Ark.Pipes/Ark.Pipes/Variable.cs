eebdd227d5453862bdfe96bfd48da5b86a6c0bdf
Alexey Volkov <ark@ark-kun.com>
2013-05-19 02:05:33 +0000

    Removed the ReadableVariable<T> class by merging its code into the Variable<T> class. Added the Variable<T>.AsReadOnly() method.
    git-svn-id: http://localhost:18080/svn/Ark.Pipes@310 94022cc4-76a7-104b-a55e-098ab84f6d6f

--- Ark.Pipes/Ark.Pipes/Variable.cs
+++ Ark.Pipes/Ark.Pipes/Variable.cs
@@ -1,18 +1,64 @@
-﻿namespace Ark.Pipes {
-    public sealed class Variable<T> : ReadableVariable<T>, IIn<T> {
-        public Variable() { }
+﻿using System;
 
-        public Variable(T value)
-            : base(value) {
+namespace Ark.Pipes {
+    public sealed class Variable<T> :
+#if NOTIFICATIONS_DISABLE
+        Provider<T>
+#else
+        ProviderWithNotifier<T>
+#endif
+        , IIn<T>
+    {
+        T _value;
+
+        public Variable() {
+            _value = default(T);
+        }
+
+        public Variable(T value) {
+                            _value = value;
+#if !NOTIFICATIONS_DISABLE
+                _notifier.SetReliability(true);
+#endif
         }
 
         public new T Value {
             get { return GetValue(); }
-            set { base.SetValue(value); }
+            set { SetValue(value);
+            }
         }
 
-        public new void SetValue(T value) {
-            base.SetValue(value);
+        public override T GetValue() {
+            return _value;
+        }
+
+        public void SetValue(T value) {
+            _value = value;
+#if !NOTIFICATIONS_DISABLE
+            _notifier.SignalValueChanged();
+#endif
+        }
+
+        public Provider<T> AsReadOnly() {
+            return new ReadOnlyVariable(this);
+        }
+
+        sealed class ReadOnlyVariable : Provider<T> {
+            Variable<T> _parent;
+
+            public ReadOnlyVariable(Variable<T> parent) {
+                _parent = parent;
+            }
+
+            public override T GetValue() {
+                return _parent._value;
+            }
+
+#if !NOTIFICATIONS_DISABLE
+            public override INotifier Notifier {
+                get { return _parent.Notifier; }
+            }
+#endif
         }
     }
 }
